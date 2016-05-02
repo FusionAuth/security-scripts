f781980eb54fb7a139f44a5b945b3b27b4623ebf
Brian Pontarelli <brian@inversoft.com>
2016-05-02 16:57:19 -0600

    Added MySQL backup script and modified Monit script to handle Exceptions and close connections properly

---
+++ backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BACKUP_DIR=/tmp/backups
+DATE=`date +%Y%m%d`
+BACKUP_FILE="${DATE}.tar.gz"
+BACKUP_FILE_ENCRYPTED="${DATE}.tar.gz.enc"
+BACKUP_PASSPHRASE_FILE="${DATE}.passphrase"
+BACKUP_PASSPHRASE_FILE_ENCRYPTED="${DATE}.passphrase.enc"
+
+# Make the directory just in case it doesn't exist
+mkdir ${BACKUP_DIR}
+cd ${BACKUP_DIR}
+
+# Delete the oldest files by only listing out everything older than the newest 7 files
+ls *.gz.enc | sort | tail -n +7 | xargs rm
+ls *.gz.passphrase.enc | sort | tail -n +7 | xargs rm
+
+# Backup the MySQL databases
+mysqldump -u<username> -p<password> production > production.sql
+
+# Tar GZ everything (modify this line to include more files and directories in the backup)
+tar -pczf ${BACKUP_FILE} /etc *.sql
+
+# Generate a random passphrase
+openssl rand 32 -out ${BACKUP_PASSPHRASE_FILE}
+
+# Encrypt the backup tar.gz
+openssl enc -aes-256-cbc -pass file:${BACKUP_PASSPHRASE_FILE} < ${BACKUP_FILE} > ${BACKUP_FILE_ENCRYPTED}
+
+# Encrypt the passphrase
+openssl rsautl -encrypt -pubin -inkey ~/public-key.pem < ${BACKUP_PASSPHRASE_FILE} > ${BACKUP_PASSPHRASE_FILE_ENCRYPTED}
+
+# Clean up
+rm ${BACKUP_FILE} ${BACKUP_PASSPHRASE_FILE}
+
+# Copy offsite
+#scp ${BACKUP_FILE_ENCRYPTED} ${BACKUP_PASSPHRASE_FILE_ENCRYPTED} <username>@<backup-server>:backups/.
--- monit-slack-pushover.rb
+++ monit-slack-pushover.rb
@@ -9,28 +9,38 @@ pushover_application="%PUSHOVER_APPLICATION%"
 pushover_user="%PUSHOVER_USER%"
 pushover_enabled=%PUSHOVER_ENABLED%
 
-if slack_enabled
-  uri = URI.parse(slack_webhook_url)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'application/json'})
-  request.body = {
-      "text" => "[#{ENV['MONIT_HOST']}] #{ENV['MONIT_SERVICE']} - #{ENV['MONIT_DESCRIPTION']}"
-  }.to_json
-  response = http.request(request)
+def log(message)
   open('/var/log/monit.log', 'a') { |f|
-    f.puts "Response from Slack [#{response.code}] [#{response.body}]"
+    f.puts message
   }
 end
 
+if slack_enabled
+  begin
+    uri = URI.parse(slack_webhook_url)
+    Net::HTTP.start(uri.host, uri.port, {use_ssl: true}) { |http|
+      request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'application/json'})
+      request.body = {
+          :text => "[#{ENV['MONIT_HOST']}] #{ENV['MONIT_SERVICE']} - #{ENV['MONIT_DESCRIPTION']}"
+      }.to_json
+      response = http.request(request)
+      log("Response from Slack [#{response.code}] [#{response.body}]")
+    }
+  rescue Exception => e
+    log("Exception while calling Slack [#{e.message}]")
+  end
+end
+
 if pushover_enabled
-  uri = URI.parse("https://api.pushover.net/1/messages.json")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'multipart/form-data'})
-  request.set_form_data("token" => pushover_application, "user" => pushover_user, "message" => "[#{ENV['MONIT_HOST']}] #{ENV['MONIT_SERVICE']} - #{ENV['MONIT_DESCRIPTION']}")
-  response = http.request(request)
-  open('/var/log/monit.log', 'a') { |f|
-    f.puts "Response from Pushover [#{response.code}] [#{response.body}]"
-  }
+  begin
+    uri = URI.parse("https://api.pushover.net/1/messages.json")
+    Net::HTTP.start(uri.host, uri.port, {use_ssl: true}) { |http|
+      request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' => 'multipart/form-data'})
+      request.set_form_data(token: pushover_application, user: pushover_user, message: "[#{ENV['MONIT_HOST']}] #{ENV['MONIT_SERVICE']} - #{ENV['MONIT_DESCRIPTION']}")
+      response = http.request(request)
+      log("Response from Pushover [#{response.code}] [#{response.body}]")
+    }
+  rescue Exception => e
+    log("Exception while calling Pushover [#{e.message}]")
+  end
 end
