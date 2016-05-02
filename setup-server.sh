8935ec95c0ee6e3b9b9f8d284b7be53012713550
Brian Pontarelli <brian@inversoft.com>
2016-05-02 17:40:46 -0600

    Fixing bugs in replacements

--- setup-server.sh
+++ setup-server.sh
@@ -44,14 +44,13 @@ function log_error {
   echo -e "\e[91m************** ${1} **************"
 }
 
+# Global variables suck, but so do Bash functions
 function ask_yes_no {
-  ANSWER=""
-  while [[ ${ANSWER} != "y" && ${ANSWER} != "n" ]]; do
+  answer=""
+  while [[ ${answer} != "y" && ${answer} != "n" ]]; do
     echo ${1}
-    read ANSWER
+    read answer
   done
-
-  return ${ANSWER}
 }
 
 
@@ -60,15 +59,15 @@ if [[ $# != 3 ]]; then
   exit 1
 fi
 
-ORDINARY_USER=$1
-SSH_KEY_FILE=$2
-IPTABLE_CFG_FILE=$3
+ordinary_user=$1
+ssh_key_file=$2
+iptable_cfg_file=$3
 
-if ! [ -f ${SSH_KEY_FILE} ]; then
+if ! [ -f ${ssh_key_file} ]; then
   bail "Invalid SSH public key file"
 fi
 
-if ! [ -f ${IPTABLE_CFG_FILE} ]; then
+if ! [ -f ${iptable_cfg_file} ]; then
   bail "Invalid IPTables configuration file"
 fi
 
@@ -82,29 +81,29 @@ fi
 
 ####### Start SSH Setup #######
 echo "Adding the ordinary user"
-if useradd -m -G sudo -s /bin/bash ${ORDINARY_USER}; then
-  echo "Please provide the password for the ${ORDINARY_USER}"
-  if ! passwd ${ORDINARY_USER}; then
+if useradd -m -G sudo -s /bin/bash ${ordinary_user}; then
+  echo "Please provide the password for the ${ordinary_user}"
+  if ! passwd ${ordinary_user}; then
     bail "Unable to change the ordinary user's password"
   fi
 
-  if ! mkdir -p /home/${ORDINARY_USER}/.ssh; then
+  if ! mkdir -p /home/${ordinary_user}/.ssh; then
     bail "Unable to create .ssh directory for the ordinary user"
   fi
 
-  if ! cp ${SSH_KEY_FILE} /home/${ORDINARY_USER}/.ssh/authorized_keys; then
+  if ! cp ${ssh_key_file} /home/${ordinary_user}/.ssh/authorized_keys; then
     bail "Unable to create .ssh directory for the ordinary user"
   fi
 
-  if ! chown -R ${ORDINARY_USER}:${ORDINARY_USER} /home/${ORDINARY_USER}/.ssh; then
+  if ! chown -R ${ordinary_user}:${ordinary_user} /home/${ordinary_user}/.ssh; then
     bail "Unable to chown the ordinary user's SSH files"
   fi
 
-  if ! chmod 700 /home/${ORDINARY_USER}/.ssh; then
+  if ! chmod 700 /home/${ordinary_user}/.ssh; then
     bail "Unable to chmod the ordinary user's .ssh directory"
   fi
 
-  if ! chmod 600 /home/${ORDINARY_USER}/.ssh/authorized_keys; then
+  if ! chmod 600 /home/${ordinary_user}/.ssh/authorized_keys; then
     bail "Unable to chmod the ordinary user's SSH authorized_keys file"
   fi
 else
@@ -145,11 +144,11 @@ if ! apt-get -qq -y install iptables-persistent; then
 fi
 
 cp /etc/iptables/rules.v4 /etc/iptables/rules.v4.orig
-cp ${IPTABLE_CFG_FILE} /etc/iptables/rules.v4
+cp ${iptable_cfg_file} /etc/iptables/rules.v4
 if grep "%APPLICATION_SERVER_IP%" /etc/iptables/rules.v4 > /dev/null; then
   echo "Enter the IP address of the Application Server"
-  read APPLICATION_SERVER_IP
-  sed -i.bak "s/%APPLICATION_SERVER_IP%/${APPLICATION_SERVER_IP}/g" /etc/iptables/rules.v4
+  read application_server_ip
+  sed -i.bak "s/%APPLICATION_SERVER_IP%/${application_server_ip}/g" /etc/iptables/rules.v4
   rm /etc/iptables/rules.v4.bak
 fi
 
@@ -161,60 +160,64 @@ fi
 ####### Start Monit Setup #######
 echo "Installing Monit for login/intrusion detection"
 if apt-get -qq -y install monit > /dev/null 2>&1; then
-  if ! grep "^set alert ${MONIT_EMAIL}" /etc/monit/monitrc > /dev/null 2>&1; then
-    echo "Enter the SMTP host"
-    read SMTP_HOST
-    echo "Enter the SMTP port"
-    read SMTP_PORT
-    echo "Enter the SMTP username"
-    read SMTP_USERNAME
-    echo "Enter the SMTP password"
-    read SMTP_PASSWORD
+  echo "Enter email address to send alerts to"
+  read monit_alert_email
 
-    SMTP_ENCRYPTION=""
-    while [[ ${SMTP_ENCRYPTION} != "ssl" && ${SMTP_ENCRYPTION} != "tlsv12" ]]; do
+  if ! grep "^set alert ${monit_alert_email}" /etc/monit/monitrc > /dev/null 2>&1; then
+    echo "Enter the SMTP host"
+    read smtp_host
+    echo "Enter the SMTP port"
+    read smtp_port
+    echo "Enter the SMTP username"
+    read smtp_username
+    echo "Enter the SMTP password"
+    read smtp_password
+
+    smtp_encryption=""
+    while [[ ${smtp_encryption} != "ssl" && ${smtp_encryption} != "tlsv12" ]]; do
       echo "Enter the SMTP encryption (ssl or tlsv12 - older versions of Monit require tlsv12 to work properly)"
-      read SMTP_ENCRYPTION
+      read smtp_encryption
     done
 
-    echo "Enter email address to send alerts to"
-    read MONIT_EMAIL
-
     cp /etc/monit/monitrc /etc/monit/monitrc.orig
-    replace_uncomment_or_append /etc/monit/monitrc "^set mailserver.*${SMTP_HOST}" "set mailserver.*" "set mailserver ${SMTP_HOST} port ${SMTP_PORT} username \"${SMTP_USERNAME}\" password \"${SMTP_PASSWORD}\" using ${SMTP_ENCRYPTION}"
-    echo "set alert ${MONIT_EMAIL} not on { instance, action }" >> /etc/monit/monitrc
+    replace_uncomment_or_append /etc/monit/monitrc "^\s*set daemon 5" "\s*set daemon .*" "set daemon 5"
+    replace_uncomment_or_append /etc/monit/monitrc "^set mailserver.*${smtp_host}" "set mailserver.*" "set mailserver ${smtp_host} port ${smtp_port} username \"${smtp_username}\" password \"${smtp_password}\" using ${smtp_encryption}"
+    echo "set alert ${monit_alert_email} not on { instance, action }" >> /etc/monit/monitrc
 
     if ! service monit restart; then
       log_error "Unable to restart Monit. Everything appears to be okay otherwise. You'll just need to figure out why the reload failed"
     fi
 
     ask_yes_no "Install Ruby and Monit Slack/Pushover integration? (y/n)"
-    if [[ ${?} == "y" ]]; then
-      if ! apt-get install ruby; then
+    if [[ "${answer}" == "y" ]]; then
+      if ! apt-get -qq -y install ruby > /dev/null 2>&1; then
         log_error "Unable to install Ruby. Skipping Monit Slack/Pushover integration"
       else
         cp monit-ssh-logins-exec.cfg /etc/monit/conf.d/ssh-logins
 
         ask_yes_no "Enable Slack notifications? (y/n)"
-        SLACK_WEBHOOK_URL="Not-Enabled"
-        if [[ ${?} == "y" ]]; then
+        slack_webhook_url="Not-Enabled"
+        slack_enabled="false"
+        if [[ "${answer}" == "y" ]]; then
           echo "Enter the Slack Webhook URL (i.e. https://hooks.slack.com/services/A0411FLaa/B004CKBBB/E7eeeea2a7a1U6EUhnIAus6z)"
-          read SLACK_WEBHOOK_URL
-          SLACK_ENABLED="true"
+          read slack_webhook_url
+          slack_enabled="true"
         fi
 
         ask_yes_no "Enable Pushover notifications? (y/n)"
-        PUSHOVER_APPLICATION="Not-Enabled"
-        PUSHOVER_USER="Not-Enabled"
-        if [[ ${?} == "y" ]]; then
+        pushover_application="Not-Enabled"
+        pushover_user="Not-Enabled"
+        pushover_enabled="false"
+        if [[ "${answer}" == "y" ]]; then
           echo "Enter the Pushover Application key"
-          read PUSHOVER_APPLICATION
+          read pushover_application
           echo "Enter the Pushover user/group key"
-          read PUSHOVER_USER
-          SLACK_ENABLED="true"
+          read pushover_user
+          pushover_enabled="true"
         fi
 
-        sed "s/%SLACK_WEBHOOK_URL%/${SLACK_WEBHOOK_URL}/g;s/%PUSHOVER_APPLICATION%/${PUSHOVER_APPLICATION}/g;s/%PUSHOVER_USER%/${PUSHOVER_USER}/g" < monit-slack-pushover.rb > /etc/monit/monit-slack-pushover.rb
+echo "s/%SLACK_ENABLED%/${slack_enabled}/g;s/%SLACK_WEBHOOK_URL%/${slack_webhook_url}/g;s/%PUSHOVER_ENABLED%/${pushover_enabled}/g;s/%PUSHOVER_APPLICATION%/${pushover_application}/g;s/%PUSHOVER_USER%/${pushover_user}/g"
+        sed "s/%SLACK_ENABLED%/${slack_enabled}/g;s/%SLACK_WEBHOOK_URL%/${slack_webhook_url}/g;s/%PUSHOVER_ENABLED%/${pushover_enabled}/g;s/%PUSHOVER_APPLICATION%/${pushover_application}/g;s/%PUSHOVER_USER%/${pushover_user}/g" < monit-slack-pushover.rb > /etc/monit/monit-slack-pushover.rb
         chmod +x /etc/monit/monit-slack-pushover.rb
       fi
     else
