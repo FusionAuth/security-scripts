4587eac07ba9867b8acd4db538aa4f37695d0a89
Brian Pontarelli <brian@inversoft.com>
2016-06-21 16:05:34 -0600

    Moved check for required attributes to the metadata.rb file.

--- chef-repo/cookbooks/2016_security_guide/metadata.rb
+++ chef-repo/cookbooks/2016_security_guide/metadata.rb
@@ -5,3 +5,13 @@ license 'all_rights'
 description 'Installs/Configures 2016_security_guide'
 long_description 'Installs/Configures 2016_security_guide'
 version '0.1.0'
+attribute 'security_guide/users',
+          :display_name => 'Users',
+          :description => 'List of the users to create on the server',
+          :type => 'array',
+          :required => 'required'
+attribute 'security_guide/monit/alert_email',
+          :display_name => 'Monit Alert Email',
+          :description => 'The email where Monit sends alerts',
+          :type => 'string',
+          :required => 'required'
